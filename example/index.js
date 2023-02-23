83db0af8469aff869c7f23873d4b1bc637efb955
Gökay Solman <gokaysolman@hotmail.com>
2023-02-23 15:35:39 +0300

    Files added

---
+++ example/index.js
@@ -0,0 +1,97 @@
+const { Sequelize, DataTypes, Model } = require('../index.js')(require('sequelize'));
+
+class Book extends Model {}
+class Category extends Model {}
+class Store extends Model {}
+
+const database = new Sequelize({
+  logging: false,
+  dialect: 'sqlite',
+  storage: __dirname +'/bookstore.sqlite'
+});
+
+Book.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING
+  },
+  categoryId: {
+    type: DataTypes.INTEGER
+  },
+  storeId: {
+    type: DataTypes.INTEGER
+  }
+}, {
+  sequelize: database,
+  tableName: 'books',
+  timestamps: false
+});
+
+Store.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING
+  }
+}, {
+  sequelize: database,
+  tableName: 'stores',
+  timestamps: false
+});
+
+Category.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING
+  },
+  parentId: {
+    type: DataTypes.INTEGER
+  }
+}, {
+  sequelize: database,
+  tableName: 'categories',
+  timestamps: false
+});
+
+// "as" property is important! 
+
+Book.belongsTo(Category, { as: 'category', foreignKey: 'categoryId' });
+Book.belongsTo(Store, { as: 'store', foreignKey: 'storeId' });
+
+Category.hasMany(Book, { as: 'books', foreignKey: 'categoryId' });
+Category.hasMany(Category, { as: 'subs', foreignKey: 'parentId' });
+Category.belongsTo(Category, { as: 'parent', foreignKey: 'parentId' });
+
+Store.hasMany(Book, { as: 'books', foreignKey: 'storeId' });
+
+(async () => {
+
+  const queries = [
+    () => Category.findAll({ include: '!books' }), // Category must have any book
+    () => Category.findAll({ include: '?books' }), // Category could not have any book
+    () => Category.findAll({ include: 'subs:5' }), // Recursive association
+    () => Category.findAll({ include: 'subs.books.store' }), // Nested associations
+    () => Category.findAll({ include: 'subs:5.books' }), // Recursive nested association
+    () => Category.findAll({ include: [ 'books.store', 'parent' ] }), // Multiple associations
+    () => Category.findAll({ include: ['!books', '?parent'] }), // Category must have any book, but dont need parent category
+    () => Category.findAll({ include: { 'parent.books': true, 'subs:5.books': { where: { id: 6 }, limit: 1 } } }), // keys are associations
+    () => Category.findAll({ include: { 'parent.books': true, 'subs:5.books': () => ({ where: { id: 6 }, limit: 1 }) } }), // association value can be function
+  ];
+
+  for (const result of queries) {
+    console.log('-------------------');
+    console.log(JSON.stringify(await result(), null, 2));  
+  }
+
+}).call(null);
---
+++ index.js
@@ -0,0 +1,121 @@
+module.exports = (sequelize) => {
+
+  Object.isObject = (o) => typeof o === 'object';
+  Function.isFunction = (f) => typeof f === 'function' && typeof f.prototype === 'undefined';
+
+  class Model extends sequelize.Model
+  {
+    static _parseAssociations(association, extra = {}){
+      if (typeof association === 'string') {
+        const detail = /([!\?]*)?([^:\.]+):?([0-9]*)\.?(.+)?/g.exec(association);
+
+        const assoc = {
+          requrired: detail[1],
+          name: detail[2],
+          depth: (detail[3] - 1) || 0,
+          remain: detail[4]
+        };
+
+        if (this.associations[assoc.name]) {
+          const _model = this.associations[assoc.name].target;
+          const includes = (() => {
+
+            if (extra.include && !assoc.remain) {
+              extra.include = _model._parseAssociations(extra.include);
+            }
+
+            if (!assoc.depth) {
+              return _model._parseAssociations(assoc.remain, extra);
+            }
+
+            const subAssociations = [];
+            assoc.remain && subAssociations.push(assoc.remain);
+
+            if (assoc.depth > 0) {
+              const subSubs = [ assoc.name+':'+ (assoc.depth - 1) ];
+              assoc.remain && subSubs.push(assoc.remain);
+              subAssociations.push(subSubs.join('.'));
+            }
+
+            return _model._parseAssociations(subAssociations, extra);
+          }).call(null);
+
+          assoc.remain && ( extra = {} );
+
+          return {
+            as: assoc.name,
+            model: _model,
+            ...(assoc.requrired ? { required: (assoc.requrired === '!') } : {}),
+            ...(includes ? { include: includes } : {}),
+            ...extra
+          };
+        }
+
+      } else if (Array.isArray(association)) {
+
+        association = association.map(assoc => this._parseAssociations(assoc, extra));
+
+      } else if (Object.isObject(association)) {
+
+        const associations = [];
+
+        for (const key of Object.keys(association)) {
+          if (Function.isFunction(association[key])) {
+            associations.push(this._parseAssociations(key, association[key].call(null)));
+          } else if (association[key] === true) {
+            associations.push(this._parseAssociations(key));
+          } else if (Object.isObject(association[key])) {
+            associations.push(this._parseAssociations(key, association[key]));
+          }
+        }
+
+        association = associations;
+      }
+
+      return association;
+    }
+    
+    static _updateOptions(options){
+      if (options.include) {
+       options.include = this._parseAssociations(options.include);
+      }
+      
+      return options;
+    }
+
+    static findOne(options){
+      return super.findOne(this._updateOptions(options));
+    }
+
+    static findAll(options){
+      return super.findAll(this._updateOptions(options));
+    }
+
+    static findAndCountAll(options){
+      return super.findAll(this._updateOptions(options)); 
+    }
+
+    static count(options){
+      return super.count(this._updateOptions(options)); 
+    }
+  }
+
+  class Sequelize extends sequelize.Sequelize
+  {
+    define(modelName, attributes = {}, options = {}) {
+      options.modelName = modelName;
+      options.sequelize = this;
+
+      const model = class extends Model {};
+
+      model.init(attributes, options);
+
+      return model;
+    }
+  }
+
+  Sequelize.Model = Model;
+  Sequelize.Sequelize = Sequelize;
+  
+  return Sequelize;
+};
